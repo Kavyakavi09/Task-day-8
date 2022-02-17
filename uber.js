9f501ced18370da19e742def1066ea9a8e89c1a2
Kavya <kavyakaruppusamy09@gmail.com>
2022-02-17 04:19:02 -0600

    uber

--- uber.js
+++ uber.js
@@ -1,21 +1,27 @@
 
 
-var taxiFare = function (milesTraveled, pickupTime) {
+class UberFare{
+	constructor(distance,pickupTime){
+        this.distance = distance;
+		this.pickupTime =pickupTime;
+	}
+
+	taxiFare(){
 	var baseFare = 2.5;
 	var costPerKM = 20.0;
 	var nightSurcharge = 0.5; // 8pm to 6am, every night
 
-	var cost = baseFare + costPerKM * milesTraveled;
+	var cost = baseFare + costPerKM * this.distance;
 
 	// add the nightSurcharge to the cost if it is after
 	// 8pm or before 6am
-	if (pickupTime >= 20 || pickupTime < 6) {
+	if (this.pickupTime >= 20 || this.pickupTime < 6) {
 		cost += nightSurcharge;
 	}
 
 	return cost;
-};
-// calculates taxi fare based upon miles traveled
-// and the hour of the day in military time (0-23).
-let tripCost = taxiFare(7, 4);
-console.log(tripCost);
+	}
+}
+
+let Uberfare = new UberFare(8,4);
+console.log(Uberfare.taxiFare());
